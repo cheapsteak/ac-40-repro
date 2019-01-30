42e581dc5946c09a9e2db049feedf33ddefb682b
Chang Wang <garnwraly@gmail.com>
2019-01-30 16:30:25 -0500

    working repro?

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag';
+import { Query, withApollo } from 'react-apollo';
 
 class App extends Component {
+  state = {
+    shouldShowQuery: true,
+  };
+  componentDidMount() {
+    console.log('resetStore 1: start');
+    this.props.client.resetStore()
+      .then(() => console.log('resetStore 1: done'))
+      .catch(e => {
+        console.error('resetStore 1: failed', e);
+      });
+
+    setTimeout(() => {
+      console.log('calling setState');
+      // this removes the Query from render and triggers its fetch to be cancelled
+      this.setState({ shouldShowQuery: false }, () => {
+        console.log('setState callback');
+        console.log('resetStore 2: start');
+        setTimeout(() => this.props.client
+          .resetStore()
+          .then(() => console.log('resetStore 2: done'))
+          .catch(e => console.error('resetStore 2: failed', e))
+          , 1000
+        );
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        {this.state.shouldShowQuery && (
+          <Query
+            query={gql`
+              query NetworkQuery {
+                allUsers {
+                  id
+                  name
+                }
+              }
+            `}
           >
-            Learn React
-          </a>
-        </header>
+            {({ data, loading, error }) =>
+              JSON.stringify({ data, loading, error })
+            }
+          </Query>
+        )}
       </div>
     );
   }
 }
 
-export default App;
+export default withApollo(App);
